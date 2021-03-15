56280d440c12e88946c6d6fc9c87a7185730ee3e
fannybouillet <49002409+fannybouillet@users.noreply.github.com>
2021-03-15 16:35:13 +0100

    Commentaires

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // barre d'application
         appBar: AppBar(
             title:Text('Happy Animal')),
         body: SafeArea(
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Column(
+                  // regarder une vidéo
                   children: [
                     Text('Regarder une vidéo pour aider'),
                     Image.network(
@@ -30,14 +32,14 @@ class MyApp extends StatelessWidget {
                     Text('Blabla')
                   ],
                 ),
-                Text('Les refuges autour de moi'),
+                //Les refuges
+                Text('Les refuges qui ont besoin de moi'),
               HomePageRow(buttonText: 'Voir tous les refuges',
                 image:  Image.network('https://img-19.ccm2.net/8vUCl8TXZfwTt7zAOkBkuDRHiT8=/1240x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg',
                   fit:BoxFit.cover,
                   width: 200,
                 ),
-                name: 'The XXX',
-                subtitle: 'Manque X repas',),
+                name: 'The XXX',),
               ],
             ),
           ),
